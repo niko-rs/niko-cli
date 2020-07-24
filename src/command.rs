a0ca6efaa0db5f52405c57838a632f5cc7183546
Vengarioth | Andreas Fischer <opensource@deviru.de>
2020-07-25 01:42:27 +0200

    improve cli ux

--- src/command.rs
+++ src/command.rs
@@ -4,6 +4,7 @@ use structopt::StructOpt;
 #[derive(Debug, StructOpt)]
 #[structopt(name = "niko", about = "command line tool for the niko game engine")]
 pub enum Command {
+    #[structopt(about = "Creates a new project as a sub-directory with the given <name>")]
     New {
         #[structopt(name = "NAME", help = "the name of the project")]
         name: String,
@@ -14,7 +15,14 @@ pub enum Command {
         #[structopt(name = "PATH", short = "p", long = "path", help = "the path to create the project in")]
         path: Option<PathBuf>,
     },
+    #[structopt(about = "Creates a new project in the current directory and uses the directory name as the project name")]
+    Init {
+        #[structopt(name = "AUTHOR", short = "a", long = "author", help = "the author put into Cargo.toml")]
+        author: Option<String>,
+    },
+    #[structopt(about = "Runs the development server and opens the project in the system's configured browser")]
     Watch,
+    #[structopt(about = "Bundles the project as a release that you can upload to itch.io")]
     Bundle,
 }
 
--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,31 @@ pub async fn main() {
     match Command::parse() {
         Command::New { name, author, path, } => {
 
-            let path = if let Some(path) = path {
+            let mut path = if let Some(path) = path {
                 path
             } else if let Some(path) = std::env::current_dir().ok() {
+                path
+            } else {
+                panic!("path was not specified and could not be inferred");
+            };
+
+            // add the name on top of the path
+            path.push(&name);
+
+            let author = if let Some(author) = author {
+                author
+            } else if let Some(author) = projects::find_author_name() {
+                author
+            } else {
+                panic!("author was not specified and could not be inferred");
+            };
+
+            // creates a new project
+            projects::generate_project(path, name, author);
+
+        },
+        Command::Init { author, } => {
+            let path = if let Some(path) = std::env::current_dir().ok() {
                 path
             } else {
                 panic!("path was not specified and could not be inferred");
@@ -26,7 +48,12 @@ pub async fn main() {
                 panic!("author was not specified and could not be inferred");
             };
 
-            // creates a new project
+            let name = if let Some(name) = path.file_name() {
+                name.to_string_lossy().to_string()
+            } else {
+                panic!("name was not inferred from current location");
+            };
+
             projects::generate_project(path, name, author);
 
         },
